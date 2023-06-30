3f9e6c47d89901eb640b297c5274a520136b2dde
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-30 10:49:32 +0600

    latest project finish

--- src/Pages/Portfoilo.jsx
+++ src/Pages/Portfoilo.jsx
@@ -2,12 +2,25 @@ import { Box, Link, Typography } from "@mui/material";
 import HeaderTitle from "../components/HeaderTitle";
 import { styled } from "@mui/system";
 import brandFood from '../../public/images/brand-food.png';
-import sport from '../../public/images/sports-special.png'
+import sport from '../../public/images/sports-special.png';
+import photoawesome from '../../public/images/photo-awesome.png'
+import { useState } from "react";
 
 const Portfoilo = () => {
+    const [show, setShow] = useState(false)
+    const [ids,setIds]=useState(null)
 
     const projects = [
         {
+            id:1,
+            name: 'Photo Awesome',
+            img: photoawesome,
+            live: "https://photo-awesome-b8515.web.app/",
+            code_client: "https://github.com/mdasif61/photo_awesome_client",
+            code_server: "https://github.com/mdasif61/photo_awesome_server"
+        },
+        {
+            id:2,
             name: 'Sport&Special',
             img: sport,
             live: "https://sports-special-ca4dd.web.app/",
@@ -15,6 +28,7 @@ const Portfoilo = () => {
             code_server: "https://github.com/mdasif61/toy_server"
         },
         {
+            id:3,
             name: 'BrandFood.C',
             img: brandFood,
             live: "https://brand-food-b1948.web.app/",
@@ -35,8 +49,8 @@ const Portfoilo = () => {
         display: 'grid',
         gridTemplateColumns: 'repeat(3,1fr)',
         gap: '15px',
-        [theme.breakpoints.down('md')]:{
-            gridTemplateColumns:'repeat(1,1fr)'
+        [theme.breakpoints.down('md')]: {
+            gridTemplateColumns: 'repeat(1,1fr)'
         }
     }))
 
@@ -48,19 +62,17 @@ const Portfoilo = () => {
     const InnerText = styled(Box)(({ theme }) => ({
         position: 'absolute',
         width: '100%',
-        height: '100%',
+        height: '20%',
+        bottom: '0',
         color: "white",
-        opacity: '0',
+        opacity: show?"1":"0",
         transition: 'all 1s',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         cursor: 'pointer',
-        "&:hover": {
-            opacity: '1',
-            backgroundColor: "rgba(255, 68, 0, 0.8)"
-        }
+        backgroundColor: "rgba(255, 68, 0, 0.8)"
     }))
 
     return (
@@ -68,18 +80,27 @@ const Portfoilo = () => {
             <HeaderTitle title={'Latest Projects'} />
             <ShowCase>
                 {
-                    projects.map(({ name, img, live, code_client, code_server }) => (
+                    projects.map(({ name, img, live, code_client, code_server,id }) => (
                         <Link href={live} target="_blank" key={name}>
                             <Box>
-                                <ImageLayout sx={{
-                                    backgroundImage: `url(${img})`,
-                                    backgroundPosition: 'top',
-                                    backgroundSize: 'cover',
-                                    transition: 'all 7s',
-                                    '&:hover': {
-                                        backgroundPosition: 'bottom'
-                                    }
-                                }}>
+                                <ImageLayout
+                                    onMouseEnter={() => {
+                                        setShow(true),
+                                        setIds(id)
+                                    }}
+                                    onMouseLeave={() => {
+                                        setShow(false),
+                                        setIds(null)
+                                    }}
+                                    sx={{
+                                        backgroundImage: `url(${img})`,
+                                        backgroundPosition: 'top',
+                                        backgroundSize: 'cover',
+                                        transition: 'all 7s',
+                                        '&:hover': {
+                                            backgroundPosition: 'bottom'
+                                        }
+                                    }}>
                                     <InnerText>
                                         <Typography variant="h5">{name}</Typography>
                                     </InnerText>
